e5466e5b0eeb30e6a9578c1d81c82917af38895b
Triple0 <lekan.olapeju@yahoo.com>
2020-04-02 18:16:26 +0100

    easy and medium assignments in typescripts

---
+++ ts-edabit.ts
@@ -0,0 +1,133 @@
+export{};
+// https://edabit.com/challenge/wPNzJEJebN2mewcqY
+// Reverse the Order of Words with Five Letters or More
+// Published by Matt in 
+// formattingsortingstrings
+// Write a function that takes a string of one or more words as an argument and returns the same string, but with all five or more letter words reversed. Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+// Examples
+// reverse("Reverse") ➞ "esreveR"
+
+// reverse("This is a typical sentence.") ➞ "This is a lacipyt .ecnetnes"
+
+// reverse("The dog is big.") ➞ "The dog is big."
+// Notes
+
+function reverse(str: string): string {
+	let str1: Array<string> = [];
+	let str2: Array<string> = str.split(' ');
+	for(let i = 0; i < str2.length; i++){
+		if(str2[i].length > 4){
+			str1.push(str2[i].split('').reverse().join(''));			
+		} else {
+			str1.push(str2[i]);
+		}
+	}
+	return str1.join(' ');	
+	
+}
+
+console.log(reverse("This is a typical sentence."));
+
+// Hello; Hello World; World
+// Published by Matt in 
+// conditionsmathstrings
+// Write a function that takes an integer and:
+// •	If the number is a multiple of 3, return "Hello".
+// •	If the number is a multiple of 5, return "World".
+// •	If the number is a multiple of both 3 and 5, return "Hello World".
+// Examples
+// helloWorld(3) ➞ "Hello"
+
+// helloWorld(5) ➞ "World"
+
+// helloWorld(15) ➞ "Hello World"
+
+
+function helloWorld(num: number): string{
+	if(num % 3 == 0 && num %5 == 0 ){
+		return "Hello World";
+	} else if(num % 5  == 0) {
+		return "World";		
+	} else if (num % 3 == 0) {
+		return "Hello";
+	}	
+}
+
+console.log(helloWorld(3)); //➞ "Hello"
+
+console.log(helloWorld(5)); //➞ "World"
+
+console.log(helloWorld(15)); // ➞ "Hello World"
+
+// https://edabit.com/challenge/L65zKSJKaDAj6JGyt
+// Fraction Greater Than One
+// Published by Matt in 
+// language_fundamentalsmathstringsvalidation
+// Given a fraction as a string, return whether or not it is greater than 1 when evaluated.
+// Examples
+// greaterThanOne("1/2") ➞ false
+
+// greaterThanOne("7/4") ➞ true
+
+// greaterThanOne("10/10") ➞ false
+
+function greaterThanOne(frac: string): boolean {
+	return eval(frac) > 1;	
+}
+
+console.log(greaterThanOne("1/2"));//➞ false
+
+console.log(greaterThanOne("7/4"));//➞ true
+
+console.log(greaterThanOne("10/10"));//) ➞ false
+
+// https://edabit.com/challenge/y6dMjvNaPXXNKeBRQ
+// Get the Sum of All Array Elements
+// Published by felixjoykind in 
+// arrayslanguage_fundamentalsmathnumbers
+// Create a function that takes an array and returns the sum of all numbers in the array.
+// Examples
+// getSumOfItems([2, 7, 4]) ➞ 13
+
+// getSumOfItems([45, 3, 0]) ➞ 48
+
+// getSumOfItems([-2, 84, 23]) ➞ 105
+
+function getSumOfItems(arr: Array<number>): number {
+	return arr.reduce((accum: number, cValue: number) => accum + cValue);	
+}
+
+console.log(getSumOfItems([2, 7, 4])); //➞ 13
+
+console.log(getSumOfItems([45, 3, 0])); // ➞ 48
+
+console.log(getSumOfItems([-2, 84, 23])); //]) ➞ 105
+
+
+// https://edabit.com/challenge/R3649PDvoFf6wGxyg
+// Return the Middle Character(s) of a String
+// Published by Matt in 
+// formattingmathstrings
+// Create a function that takes a string and returns the middle character(s). If the word's length is odd, return the middle character. If the word's length is even, return the middle two characters.
+// Examples
+// getMiddle("test") ➞ "es"
+
+// getMiddle("testing") ➞ "t"
+
+// getMiddle("middle") ➞ "dd"
+
+function getMiddle(str: any): string {
+	console.log(str);
+	if(str.length == 1){
+		return str
+	} else if (str.length % 2 == 1){
+		return str.substring(str.length/2,(str.length/2)+1);
+	} else {
+	return str.substring((str.length/2)-1, (str.length/2)+1);
+	}	
+}
+
+console.log(getMiddle("testing"));// ➞ "t"
+console.log(getMiddle("middle"));// ➞ "dd"
+
+
